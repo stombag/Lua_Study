6281456248adc00f2436155c368e60fe7dda3b5c
stombag <85613689+stombag@users.noreply.github.com>
2026-01-02 21:47:27 +0900

    Create ex07.lua for variable and comparison examples
    Add Lua script to demonstrate variable types and comparisons

---
+++ ex07.lua
@@ -0,0 +1,14 @@
+local x = true
+x = false
+print(type(x))
+
+local a,b,c = 1,2,3
+print(a==b)
+local d = a ~=b
+print(d)
+local e = a<b
+local f = a ==  c
+print(e,f)
+local g = 'hi'
+local f = a == g
+print(g,f)
